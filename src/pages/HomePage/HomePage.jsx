1d657196c4b1b45f67112127adf407c1a3b875f7
Kirill <101974969+KirillPanasiuk9@users.noreply.github.com>
2023-02-10 01:33:15 +0300

    homePage layout done

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import NavBar from "../../components/NavBar/NavBar";
 import Footer from "../../components/Footer/Footer";
-import '../HomePage/homePage.scss'
-import {NavLink} from "react-router-dom";
-import {Button} from "@mui/material";
+import '../HomePage/homePage.scss';
+import HomePage_Button from "./components/Button/HomePage_Button";
+
+
 
 const HomePage = () => {
     return (
@@ -11,30 +12,7 @@ const HomePage = () => {
             <NavBar/>
             <h1>Boooooks</h1>
             <h3>It doesn't take much to make a bookworm happy</h3>
-            {/*<NavLink to={'/catalog'}>*/}
-                <Button
-                    variant="contained"
-                    className="homePage_button"
-                    sx={{
-                        width: 200,
-                        height: 66,
-                        backgroundColor: '#FC0A7E',
-                        marginLeft: "10%",
-                        display: "flex",
-                        justifyContent: "space-around",
-                        fontSize: 25,
-                        alignItems: "center",
-                        color: "white"
-                    }}
-                >
-                    <NavLink to={'/catalog'}>
-                    Shop
-                    <svg viewBox="0 0 57 19">
-                        <path d="M42.82 19C44.4733 15.5867 46.02 13.0267 47.46 11.32H0.820001V7.96H47.46C46.02 6.25333 44.4733 3.69333 42.82 0.279998H45.62C48.98 4.17333 52.5 7.05333 56.18 8.92V10.36C52.5 12.2267 48.98 15.1067 45.62 19H42.82Z" fill="white"/>
-                    </svg>
-                    </NavLink>
-                </Button>
-            {/*</NavLink>*/}
+            <HomePage_Button/>
             <Footer/>
         </div>
     );
---
+++ src/pages/HomePage/components/Button/HomePage_Button.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './homePage_Button.scss'
+import {NavLink} from "react-router-dom";
+
+const HomePage_Button = () => {
+    return (
+        <button
+            className='homePage_buttonWrapped'
+        >
+            <NavLink to={'/catalog'} className="homePage_button">
+                Shop
+                <svg width="57" height="19" viewBox="0 0 57 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M42.82 19C44.4733 15.5867 46.02 13.0267 47.46 11.32H0.820001V7.96H47.46C46.02 6.25333 44.4733 3.69333 42.82 0.279998H45.62C48.98 4.17333 52.5 7.05333 56.18 8.92V10.36C52.5 12.2267 48.98 15.1067 45.62 19H42.82Z" fill="#D8FFA4"/>
+                </svg>
+            </NavLink>
+        </button>
+    );
+};
+
+export default HomePage_Button;
