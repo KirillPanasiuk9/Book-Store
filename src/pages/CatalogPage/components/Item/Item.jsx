9448fd029fdbbef837162a3e87d5913bb32a2661
Kirill <101974969+KirillPanasiuk9@users.noreply.github.com>
2023-02-17 20:23:28 +0300

    item style fixed

--- src/pages/CatalogPage/components/Item/Item.jsx
+++ src/pages/CatalogPage/components/Item/Item.jsx
@@ -8,7 +8,7 @@ const Item = ({item}) => {
             <div className="item_content">
                 <img className="item_image" src={item.volumeInfo.imageLinks?.thumbnail}/>
                 <div className="item_title">{item.volumeInfo.title}</div>
-                <div className="item_author">{item.volumeInfo.authors[0]}</div>
+                {/*<div className="item_author">{item.volumeInfo.authors[0]}</div>*/}
                 <div className="item_price">$100</div>
                 <div className="item_button">Buy</div>
             </div>
--- src/redux/async/asyncActions.js
+++ src/redux/async/asyncActions.js
@@ -1,12 +1,11 @@
 import {isDataFetching ,isDataFetchingSuccess, isDataFetchingError} from "../slices/itemsListSlice";
-import {createAsyncThunk} from "@reduxjs/toolkit";
 
 
 export function fetchItemsList(search) {
     return async (dispatch) => {
         dispatch(isDataFetching())
         try {
-            const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${search}&key=AIzaSyDTQWN8aj5rJ0LgM3h-5nRh7y11EEA-L3I&maxResults=9`)
+            const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${search}&key=AIzaSyDTQWN8aj5rJ0LgM3h-5nRh7y11EEA-L3I&maxResults=40`)
             const data = await response.json()
             dispatch(isDataFetchingSuccess(data.items))
             console.log(data.items)
