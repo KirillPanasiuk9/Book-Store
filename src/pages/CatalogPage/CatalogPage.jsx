11c4232e8a2b3916f17f246b33b043f6d65ed727
Kirill <101974969+KirillPanasiuk9@users.noreply.github.com>
2023-02-18 15:42:49 +0300

    pagination added

--- src/pages/CatalogPage/CatalogPage.jsx
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -1,14 +1,26 @@
 import React, {useState} from 'react';
 import NavBar from "../../components/NavBar/NavBar";
 import Item from "./components/Item/Item";
-import './CatalogPage.scss'
+import './CatalogPage.scss';
+import './pagination.scss';
 import {useDispatch, useSelector} from "react-redux";
 import {fetchItemsList} from "../../redux/async/asyncActions";
+import ReactPaginate from "react-paginate";
 
 const CatalogPage = () => {
     const itemList = useSelector(state => state.itemsListSlice.items)
     const dispatch = useDispatch();
     const [search, setSearch] = useState("");
+
+    const [pageNumber, setPageNumber] = useState(0)
+    const itemsPerPage = 9;
+    const pagesVisited = pageNumber * itemsPerPage
+    const pageCount = Math.ceil(itemList.length / itemsPerPage)
+
+    const changePage = ({selected}) => {
+        setPageNumber(selected)
+    }
+
 
     const handleSearch = () => {
         dispatch(fetchItemsList(search))
@@ -24,12 +36,26 @@ const CatalogPage = () => {
                     type={"text"}
                     value={search}
                     onChange={event => setSearch(event.target.value)}
+                    onKeyDown={event => event.key === "Enter" ? handleSearch() : ""}
                 />
-                <button className="search_button" onClick={handleSearch}>Find</button>
+                <button className="search_button" onClick={handleSearch}>Search</button>
             </div>
             <div className="catalog_list">
-                {itemList.map(item => <Item item={item}/>)}
+                {itemList
+                    .slice(pagesVisited, pagesVisited + itemsPerPage)
+                    .map(item => <Item item={item}/>)}
             </div>
+            <ReactPaginate
+                pageCount={pageCount}
+                previousLabel={"Previous"}
+                nextLabel={"Next"}
+                onPageChange={changePage}
+                containerClassName={"paginationContainer"}
+                previousLinkClassName={"paginationButton"}
+                nextLinkClassName={"paginationButton"}
+                disabledClassName={"paginationDisable"}
+                activeClassName={"paginationActive"}
+            />
         </div>
     );
 };
--- src/pages/CatalogPage/components/Item/Item.jsx
+++ src/pages/CatalogPage/components/Item/Item.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 import './item.scss'
 
 const Item = ({item}) => {
-    // console.log(item.volumeInfo);
     return (
         <div className="item_box">
             <div className="item_content">
                 <img className="item_image" src={item.volumeInfo.imageLinks?.thumbnail}/>
                 <div className="item_title">{item.volumeInfo.title}</div>
-                {/*<div className="item_author">{item.volumeInfo.authors[0]}</div>*/}
+                <div className="item_author">{item.volumeInfo.authors}</div>
                 <div className="item_price">$100</div>
                 <div className="item_button">Buy</div>
             </div>
