fe86b2ac92ec2c07cfcd3a26b7522188c388c23a
Kirill <101974969+KirillPanasiuk9@users.noreply.github.com>
2023-02-19 14:48:23 +0300

    route to the item page added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from "./pages/HomePage/HomePage";
 import CatalogPage from "./pages/CatalogPage/CatalogPage";
 import AboutPage from "./pages/AboutPage/AboutPage";
 import CartPage from "./pages/CartPage/CartPage";
+import ItemPage from "./pages/ItemPage/ItemPage";
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Route path={'/catalog'} element={<CatalogPage />} />
           <Route path={'/about'} element={<AboutPage />} />
           <Route path={'/cart'} element={<CartPage />} />
+          <Route path={'/item'} element={<ItemPage />} />
         </Routes>
       </>
   );
