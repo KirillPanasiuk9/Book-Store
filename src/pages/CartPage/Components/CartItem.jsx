e160b4f92325e196469db1b109e7282efab7cfd0
Kirill <101974969+KirillPanasiuk9@users.noreply.github.com>
2023-03-01 22:32:05 +0300

    add link to itemPage from Cart

--- src/pages/CartPage/Components/CartItem.jsx
+++ src/pages/CartPage/Components/CartItem.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import "./cartItem.scss"
 import {useDispatch} from "react-redux";
 import {removeFromCartAction} from "../../../redux/slices/cartListSlice";
+import {Link} from "react-router-dom";
 
 const CartItem = ({item}) => {
     const dispatch = useDispatch()
     const {title, authors} = item
     const image = item.image
+    const category = item.categories
+    const description = item.description
+    const id = item.id
 
     const removeItem = () => {
         dispatch(removeFromCartAction(item))
@@ -14,7 +18,9 @@ const CartItem = ({item}) => {
 
     return (
         <div className="cartItem">
-            <img className="cart_image" src={image}/>
+            <Link to={'/item'} state={{title, authors, image, category, description, id}}>
+                <img className="cart_image" src={image}/>
+            </Link>
             <div className="cart_info">
                 <div className="cart_title">{title}</div>
                 <div className="cart_author">{authors}</div>
